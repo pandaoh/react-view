cb30e3727f5fa797e4495367144d4ecd77b3ff42
doubleam <admin@biugle.cn>
2022-05-09 15:24:52 +0800

    Feat: 调整引用插件代码

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,14 +2,14 @@
  * @Author: HxB
  * @Date: 2022-04-13 11:52:00
  * @LastEditors: DoubleAm
- * @LastEditTime: 2022-04-22 09:54:44
+ * @LastEditTime: 2022-05-09 15:24:04
  * @Description: user service
  * @FilePath: \react-view\src\services\user.ts
  */
 import XHttp from '@/tools/api/xhttp';
 
 export function testRequest() {
-  XHttp.get('/api/users').then(res => {
+  return XHttp.get('/api/users').then(res => {
     console.log('testRequest', res);
   });
 }
